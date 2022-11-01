95d390d4c73e211e6ba935d17bafebc3b91f1d23
pvpender <Nicolaymoiseev1@gmail.com>
2022-11-01 18:16:59 +0300

    in com

---
+++ tests/GitPhp/bootstrap.php
@@ -0,0 +1,31 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../libs/AssertRunner.php';
+
+Tester\Environment::setup();
+
+
+function test($cb)
+{
+	try {
+		$cb();
+
+	} catch (CzProject\GitPhp\GitException $e) {
+		$result = $e->getRunnerResult();
+
+		if ($result !== NULL) {
+			echo $result->getCommand(), "\n";
+			echo 'EXIT CODE: ', $result->getExitCode(), "\n";
+			echo "--------------\n",
+				$result->getOutputAsString(), "\n";
+
+			if ($result->hasErrorOutput()) {
+				echo "--------------\n",
+					implode("\n", $result->getErrorOutput()), "\n";
+			}
+		}
+
+		throw $e;
+	}
+}
