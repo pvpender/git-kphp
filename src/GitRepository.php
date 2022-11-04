44632dd1a7bf45decbccc85a1cf60ced79586e64
pvpender <Nicolaymoiseev1@gmail.com>
2022-11-04 22:13:20 +0300

    tet

--- src/GitRepository.php
+++ src/GitRepository.php
@@ -3,7 +3,10 @@
 	namespace pvpender\GitPhp;
 
 
-	class GitRepository
+	use DateTimeImmutable;
+    use DateTimeInterface;
+
+    class GitRepository
 	{
         protected string $repository;
 
@@ -402,9 +405,9 @@
 
 			// author date
 			$result = $this->run('log', '-1', $commitId, '--pretty=format:%ad', '--date=iso-strict');
-			$authorDate = \DateTimeImmutable::createFromFormat(\DateTime::ATOM, (string) $result->getOutputLastLine());
+			$authorDate = DateTimeImmutable::createFromFormat(DateTimeInterface::ATOM, (string) $result->getOutputLastLine());
 
-			if (!($authorDate instanceof \DateTimeImmutable)) {
+			if (!($authorDate instanceof DateTimeImmutable)) {
 				throw new GitException('Failed fetching of commit author date.', 0, NULL, $result);
 			}
 
@@ -418,9 +421,9 @@
 
 			// committer date
 			$result = $this->run('log', '-1', $commitId, '--pretty=format:%cd', '--date=iso-strict');
-			$committerDate = \DateTimeImmutable::createFromFormat(\DateTime::ATOM, (string) $result->getOutputLastLine());
+			$committerDate = DateTimeImmutable::createFromFormat(DateTimeInterface::ATOM, (string) $result->getOutputLastLine());
 
-			if (!($committerDate instanceof \DateTimeImmutable)) {
+			if (!($committerDate instanceof DateTimeImmutable)) {
 				throw new GitException('Failed fetching of commit committer date.', 0, NULL, $result);
 			}
 
--- tests/GitTest.php
+++ tests/GitTest.php
@@ -1,1 +1,41 @@
 <?php
+
+use pvpender\GitPhp\CommitId;
+use PHPUnit\Framework\TestCase;
+use pvpender\GitPhp\GitException;
+use pvpender\GitPhp\Helpers;
+use pvpender\GitPhp\Runners\MemoryRunner;
+use pvpender\GitPhp\Git;
+
+class GitTest extends TestCase{
+    public function testCommitID(){
+        $commitId = new CommitId('734713bc047d87bf7eac9674765ae793478c50d3');
+        $this->assertSame('734713bc047d87bf7eac9674765ae793478c50d3', (string) $commitId);
+        $this->assertSame('734713bc047d87bf7eac9674765ae793478c50d3', $commitId->toString());
+    }
+    public function testBranch(){
+
+    }
+    public function testExtractRepoNameFromUrl(){
+        $this->assertSame('repo', Helpers::extractRepositoryNameFromUrl('/path/to/repo.git'));
+        $this->assertSame('repo', Helpers::extractRepositoryNameFromUrl('/path/to/repo/.git'));
+        $this->assertSame('foo', Helpers::extractRepositoryNameFromUrl('host.xz:foo/.git'));
+        $this->assertSame('repo', Helpers::extractRepositoryNameFromUrl('file:///path/to/repo.git/'));
+        $this->assertSame('git-kphp', Helpers::extractRepositoryNameFromUrl('https://github.com/pvpender/git-kphp.git'));
+        $this->assertSame('git-php', Helpers::extractRepositoryNameFromUrl('git@github.com:czproject/git-php.git'));
+    }
+
+    /**
+     * @throws GitException
+     */
+    public function testGitRepoDir(){
+        /*$runner = new MemoryRunner(__DIR__);
+        $git = new Git($runner);
+        $repoA = $git->open(__DIR__);
+        $this.self::assertSame(__DIR__, $repoA->getRepositoryPath());
+        $repoA = $git->open(__DIR__ . '/.git');
+        $this.self::assertSame(__DIR__, $repoA->getRepositoryPath());
+        $repoA = $git->open(__DIR__ . '/.git/');
+        $this.self::assertSame(__DIR__, $repoA->getRepositoryPath());*/
+    }
+}
